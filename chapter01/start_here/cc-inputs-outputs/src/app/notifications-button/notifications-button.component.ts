236207a88e08d2eea2207305382d05f9bf1b47a6
MATHEMATICS TERRAINS <amankumarpandey2397@gmail.com>
2022-06-14 23:34:46 +0530

    Components communication using component @Input(s) and @Output(s)

--- chapter01/start_here/cc-inputs-outputs/src/app/notifications-button/notifications-button.component.ts
+++ chapter01/start_here/cc-inputs-outputs/src/app/notifications-button/notifications-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-notifications-button',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationsButtonComponent implements OnInit {
 
+  @Input() count = 0;
   constructor() { }
 
   ngOnInit(): void {
